8e8f84d6155625de34b784d3340820c5f514cfe8
WillEdgington <willedge037@gmail.com>
2026-02-10 17:59:17 +0000

    three more scenes added

--- wheres-wally-api/db/seeds.rb
+++ wheres-wally-api/db/seeds.rb
@@ -7,14 +7,50 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-image = Image.create!(
-  title: "Film Set"
+filmset = Image.create!(
+  title: "Dinosaurs, Spacemen, and Ghouls"
 )
 
 Character.create!([
-  { name: "Wally", x: 91.8, y: 55.7, image: image },
-  { name: "Wizard", x: 67.1, y: 72.5, image: image },
-  { name: "Wilma", x: 82.8, y: 58.1, image: image },
-  { name: "Woof", x: 80.8, y: 89.8, image: image},
-  { name: "Odlaw", x: 5.2, y: 49.6, image: image},
+  { name: "Wally", x: 91.8, y: 55.7, image: filmset },
+  { name: "Wizard", x: 67.1, y: 72.5, image: filmset },
+  { name: "Wilma", x: 82.8, y: 58.1, image: filmset },
+  { name: "Woof", x: 80.8, y: 89.8, image: filmset },
+  { name: "Odlaw", x: 5.2, y: 49.6, image: filmset },
+])
+
+musketeers = Image.create!(
+  title: "The Swashbuckling Musketeers"
+)
+
+Character.create!([
+  { name: "Wally", x: 52.8, y: 80.1, image: musketeers },
+  { name: "Wizard", x: 94.0, y: 30.0, image: musketeers },
+  { name: "Wilma", x: 43.6, y: 56.3, image: musketeers },
+  { name: "Woof", x: 18.0, y: 83.5, image: musketeers },
+  { name: "Odlaw", x: 15.1, y: 36.7, image: musketeers },
+])
+
+giants = Image.create!(
+  title: "The Unfriendly Giants"
+)
+
+Character.create!([
+  { name: "Wally", x: 17.9, y: 65.6, image: giants },
+  { name: "Wizard", x: 96.0, y: 76.8, image: giants },
+  { name: "Wilma", x: 69.1, y: 79.1, image: giants },
+  { name: "Woof", x: 2.2, y: 73.4, image: giants },
+  { name: "Odlaw", x: 58.7, y: 89.9, image: giants },
+])
+
+dive = Image.create!(
+  title: "The Deep Sea Divers"
+)
+
+Character.create!([
+  { name: "Wally", x: 66.3, y: 15.5, image: dive },
+  { name: "Wizard", x: 78.6, y: 12.7, image: dive },
+  { name: "Wilma", x: 51.8, y: 23.8, image: dive },
+  { name: "Woof", x: 93.9, y: 28.6, image: dive },
+  { name: "Odlaw", x: 30.1, y: 19.2, image: dive },
 ])
--- wheres-wally-frontend/src/scenes/index.js
+++ wheres-wally-frontend/src/scenes/index.js
@@ -1,8 +1,23 @@
 import scene1 from "../assets/scene_1.jpg";
+import scene2 from "../assets/scene_2.jpg";
+import scene3 from "../assets/scene_3.jpg";
+import scene4 from "../assets/scene_4.jpg";
 
 export const SCENES = [
-    {
-        id: 1,
-        image: scene1
-    }
+  {
+    id: 1,
+    image: scene1
+  },
+  {
+    id: 2,
+    image: scene2
+  },
+  {
+    id: 3,
+    image: scene3
+  },
+  {
+    id: 4,
+    image: scene4
+  }
 ]
